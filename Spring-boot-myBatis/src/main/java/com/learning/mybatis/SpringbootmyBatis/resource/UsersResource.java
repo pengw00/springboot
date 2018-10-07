5c72501e5aa6a26f541ef09c93bc1ba7f3bffefb
pengw00 <water685@hotmail.com>
2018-10-07 09:27:33 -0500

    First Commit

---
+++ Spring-boot-myBatis/src/main/java/com/learning/mybatis/SpringbootmyBatis/resource/UsersResource.java
@@ -0,0 +1,9 @@
+package com.learning.mybatis.SpringbootmyBatis.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/re")
+public class UsersResource {
+
+}
